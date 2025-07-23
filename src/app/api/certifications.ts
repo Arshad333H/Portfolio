d7d20b0266e35af9481775453eddc87155aab21e
Arshad333S <skmohammedarshad333@gmail.com>
2025-07-24 02:49:08 +0530

    Romve Linked in function

--- src/app/api/certifications.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "@/lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const certs = await prisma.certificate.findMany({
-    orderBy: { issueDate: "desc" }
-  });
-  return res.json(certs);
-}
